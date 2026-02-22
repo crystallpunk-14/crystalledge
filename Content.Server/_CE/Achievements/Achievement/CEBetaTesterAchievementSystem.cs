713848761635210cc5c9320905f1828f2ec4274b
Ed <edwardxperia2000@gmail.com>
2026-02-22 11:57:24 +0300

    Update CEBetaTesterAchievementSystem.cs

--- Content.Server/_CE/Achievements/Achievement/CEBetaTesterAchievementSystem.cs
+++ Content.Server/_CE/Achievements/Achievement/CEBetaTesterAchievementSystem.cs
@@ -1,5 +1,7 @@
 using Content.Shared._CE.Achievements.Prototypes;
+using Content.Shared.CCVar;
 using Content.Shared.GameTicking;
+using Robust.Shared.Configuration;
 using Robust.Shared.Prototypes;
 
 namespace Content.Server._CE.Achievements.Achievement;
@@ -7,6 +9,7 @@ namespace Content.Server._CE.Achievements.Achievement;
 public sealed class CEBetaTesterAchievementSystem : EntitySystem
 {
     [Dependency] private readonly CEAchievementsSystem _achievement = default!;
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
 
     private readonly ProtoId<CEAchievementPrototype> _proto = "BetaTester";
 
@@ -19,6 +22,10 @@ public sealed class CEBetaTesterAchievementSystem : EntitySystem
 
     private async void OnSpawnComplete(PlayerSpawnCompleteEvent ev)
     {
+        // Don't award achievements in integration tests to avoid interfering with test cleanup
+        if (_cfg.GetCVar(CCVars.DatabaseSynchronous))
+            return;
+            
         await _achievement.AddPlayerAchievementAsync(ev.Player.UserId, _proto);
     }
 }
