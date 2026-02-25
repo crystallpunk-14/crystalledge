8d3f4c412a46532a7e41613604c7c0c0bfdcd52c
Ed <96445749+TheShuEd@users.noreply.github.com>
2026-02-25 11:41:34 +0300

    Update CEWeaponResetOnEquipComponent.cs

--- Content.Shared/_CE/Weapon/Core/Components/CEWeaponResetOnEquipComponent.cs
+++ Content.Shared/_CE/Weapon/Core/Components/CEWeaponResetOnEquipComponent.cs
@@ -3,7 +3,8 @@ using Robust.Shared.GameStates;
 namespace Content.Shared._CE.Weapon.Core.Components;
 
 /// <summary>
-/// TODO
+/// Component that marks a weapon to have its state reset when it is equipped.
+/// The reset can only occur at most once per <see cref="Cooldown"/> interval.
 /// </summary>
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 [Access(typeof(CESharedWeaponSystem))]
