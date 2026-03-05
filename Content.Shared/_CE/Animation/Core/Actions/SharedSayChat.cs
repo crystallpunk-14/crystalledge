77142c111656b07851a8dac45b22eaee2325ff71
Ed <96445749+TheShuEd@users.noreply.github.com>
2026-03-05 19:19:43 +0300

    Update SharedSayChat.cs

--- Content.Shared/_CE/Animation/Core/Actions/SharedSayChat.cs
+++ Content.Shared/_CE/Animation/Core/Actions/SharedSayChat.cs
@@ -6,10 +6,10 @@ namespace Content.Shared._CE.Animation.Core.Actions;
 public abstract partial class SharedSayChat : CEAnimationActionEntry
 {
     /// <summary>
-    /// A message spoken by a character.
+    /// A message spoken by a character. Will automatically attempt to use it as LocId, but you can also insert regular text.
     /// </summary>
     [DataField(required: true)]
-    public LocId Sentence;
+    public string Sentence;
 
     [DataField]
     public InGameICChatType ChatType = InGameICChatType.Speak;
