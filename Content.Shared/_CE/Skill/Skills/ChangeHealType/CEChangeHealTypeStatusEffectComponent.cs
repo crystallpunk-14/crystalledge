6cdc6f9e78c13f918a0fe6ee92c779df7e4ec065
Benjamin <benjamin.schleiss@twofold.swiss>
2026-03-05 14:19:54 +0100

    fixed comment

--- Content.Shared/_CE/Skill/Skills/ChangeHealType/CEChangeHealTypeStatusEffectComponent.cs
+++ Content.Shared/_CE/Skill/Skills/ChangeHealType/CEChangeHealTypeStatusEffectComponent.cs
@@ -5,7 +5,7 @@ using Robust.Shared.Prototypes;
 namespace Content.Shared._CE.Skill.Skills.EffectiveHeal;
 
 /// <summary>
-/// Increases all outgoing healing from the player by X
+/// Replaces healing with damage of the specified <see cref="Target"/> damage type.
 /// </summary>
 [RegisterComponent, NetworkedComponent]
 public sealed partial class CEChangeHealTypeStatusEffectComponent : Component
