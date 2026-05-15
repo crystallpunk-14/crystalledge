1f2def56586e1ba319e1bfa7c56e88a536b6199a
Ed <96445749+TheShuEd@users.noreply.github.com>
2026-05-15 17:10:36 +0300

    boss wall

---
+++ Content.Shared/_CE/EntityEffect/Effects/Delete.cs
@@ -0,0 +1,17 @@
+﻿namespace Content.Shared._CE.EntityEffect.Effects;
+
+
+public sealed partial class Delete : CEEntityEffectBase<Delete>
+{
+}
+
+public sealed partial class CEQueueDelEffectSystem : CEEntityEffectSystem<Delete>
+{
+    protected override void Effect(ref CEEntityEffectEvent<Delete> args)
+    {
+        if (ResolveEffectEntity(args.Args, args.Effect.EffectTarget) is not { } entity)
+            return;
+
+        QueueDel(entity);
+    }
+}
