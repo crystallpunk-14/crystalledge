4e571dd9bcf3f2dcb372632e6da800eea21a001c
Ed <edwardxperia2000@gmail.com>
2026-06-12 14:54:07 +0300

    fixes 3

--- Content.Shared/_CE/Trading/Offers/CETradingSpawnFromCategoryOffer.cs
+++ Content.Shared/_CE/Trading/Offers/CETradingSpawnFromCategoryOffer.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+using Content.Shared._CE.Loot;
 using Content.Shared._CE.Tag;
 using Content.Shared._CE.Trading.Components;
 using Content.Shared.Hands.EntitySystems;
@@ -30,24 +30,51 @@ public sealed partial class CETradingSpawnFromCategoryOffer : CETradingOffer
 
     public override void UpdateSlotVisuals(EntityUid slotEntity, IEntityManager entMan, IPrototypeManager proto, IRobustRandom random)
     {
-        var pool = new List<EntProtoId>();
+        var pool = new List<(EntProtoId Id, float Weight)>();
+
         foreach (var entProto in proto.EnumeratePrototypes<EntityPrototype>())
         {
             if (entProto.Abstract)
                 continue;
-            if (!entProto.Components.TryGetValue("CETradingCategory", out var entry))
+            if (!entProto.Components.TryGetValue("CELootCategory", out var compEntry))
                 continue;
-            if (entry.Component is not CETradingCategoryComponent cat)
+            if (compEntry.Component is not CELootCategoryComponent cat)
                 continue;
-            if (!cat.Tags.Overlaps(Tags))
+
+            var weight = 0f;
+            foreach (var tagEntry in cat.Tags)
+            {
+                if (Tags.Contains(tagEntry.Tag))
+                    weight += tagEntry.Weight;
+            }
+
+            if (weight <= 0f)
                 continue;
-            pool.Add(new EntProtoId(entProto.ID));
+
+            pool.Add((new EntProtoId(entProto.ID), weight));
         }
 
         if (pool.Count == 0)
             return;
 
-        SelectedEntity = random.Pick(pool);
+        var totalWeight = 0f;
+        foreach (var (_, w) in pool)
+            totalWeight += w;
+
+        var roll = random.NextFloat() * totalWeight;
+        var cumulative = 0f;
+        var picked = pool[^1].Id;
+        foreach (var (id, w) in pool)
+        {
+            cumulative += w;
+            if (roll <= cumulative)
+            {
+                picked = id;
+                break;
+            }
+        }
+
+        SelectedEntity = picked;
 
         if (!proto.TryIndex<EntityPrototype>(SelectedEntity.Value, out var selected))
             return;
