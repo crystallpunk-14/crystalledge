c83c9e12a980386cd01e12f63390ad9d07be7d91
Deserty0 <86846189+Deserty0@users.noreply.github.com>
2026-06-23 18:55:01 +1000

    Change bonus [Stat] effects  (#306)
    * watch world burn
    
    * oopsie
    
    * ueueue
    
    * fix
    
    * sorry tests

---
+++ Content.Shared/_CE/StatusEffects/CEChangeMaxHealth.cs
@@ -0,0 +1,76 @@
+﻿using Content.Shared._CE.Health;
+using Content.Shared._CE.StatusEffects.Core;
+using Content.Shared._CE.StatusEffects.Core.Components;
+using Content.Shared.StatusEffectNew;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._CE.StatusEffects;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class CEChangeMaxHealthComponent : Component
+{
+    /// <summary>
+    /// Changes max health by flat amount (can be negative)
+    /// </summary>
+    [DataField]
+    public int FlatChange = 10;
+
+    /// <summary>
+    /// Changes max health by flat amount per stack (can be negative)
+    /// </summary>
+    [DataField]
+    public int FlatChangePerStack = 0;
+
+    /// <summary>
+    /// Added to global multiplier (can be negative)
+    /// </summary>
+    [DataField]
+    public float MultiplierChange = 0;
+
+    /// <summary>
+    /// Added to global multiplier per stack (can be negative)
+    /// </summary>
+    [DataField]
+    public float MultiplierChangePerStack = 0;
+}
+
+public sealed partial class CEChangeMaxHealthSystem : EntitySystem
+{
+    [Dependency] private CEMobStateSystem _mobState = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<CEChangeMaxHealthComponent, StatusEffectAppliedEvent>(OnApply);
+        SubscribeLocalEvent<CEChangeMaxHealthComponent, StatusEffectRemovedEvent>(OnRemoved);
+        SubscribeLocalEvent<CEChangeMaxHealthComponent, CEStatusEffectStackEditedEvent>(OnStackEdited);
+        SubscribeLocalEvent<CEChangeMaxHealthComponent, StatusEffectRelayedEvent<CECalculateMaxHealthEvent>>(OnCalculateMaxHealth);
+    }
+
+    private void OnApply(Entity<CEChangeMaxHealthComponent> ent, ref StatusEffectAppliedEvent args)
+    {
+        _mobState.RefreshMaxHealth(args.Target);
+    }
+
+    private void OnRemoved(Entity<CEChangeMaxHealthComponent> ent, ref StatusEffectRemovedEvent args)
+    {
+        _mobState.RefreshMaxHealth(args.Target);
+    }
+
+    private void OnStackEdited(Entity<CEChangeMaxHealthComponent> ent, ref CEStatusEffectStackEditedEvent args)
+    {
+        _mobState.RefreshMaxHealth(args.Target);
+    }
+
+    private void OnCalculateMaxHealth(Entity<CEChangeMaxHealthComponent> ent,
+        ref StatusEffectRelayedEvent<CECalculateMaxHealthEvent> args)
+    {
+        var stacks = 1;
+        if (TryComp<CEStatusEffectStackComponent>(ent, out var stackComp))
+            stacks = stackComp.Stacks;
+
+        args.Args.FlatModifier += ent.Comp.FlatChange + ent.Comp.FlatChangePerStack * stacks;
+        args.Args.Multiplier += ent.Comp.MultiplierChange + ent.Comp.MultiplierChangePerStack * stacks;
+    }
+}
---
+++ Content.Shared/_CE/StatusEffects/CEChangeMaxStamina.cs
@@ -0,0 +1,76 @@
+﻿using Content.Shared._CE.Stamina;
+using Content.Shared._CE.StatusEffects.Core;
+using Content.Shared._CE.StatusEffects.Core.Components;
+using Content.Shared.StatusEffectNew;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._CE.StatusEffects;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class CEChangeMaxStaminaComponent : Component
+{
+    /// <summary>
+    /// Changes max health by flat amount (can be negative)
+    /// </summary>
+    [DataField]
+    public int FlatChange = 10;
+
+    /// <summary>
+    /// Changes max health by flat amount per stack (can be negative)
+    /// </summary>
+    [DataField]
+    public int FlatChangePerStack = 0;
+
+    /// <summary>
+    /// Added to global multiplier (can be negative)
+    /// </summary>
+    [DataField]
+    public float MultiplierChange = 0;
+
+    /// <summary>
+    /// Added to global multiplier per stack (can be negative)
+    /// </summary>
+    [DataField]
+    public float MultiplierChangePerStack = 0;
+}
+
+public sealed partial class CEChangeMaxStaminaSystem : EntitySystem
+{
+    [Dependency] private CEStaminaSystem _stamina = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<CEChangeMaxStaminaComponent, StatusEffectAppliedEvent>(OnApply);
+        SubscribeLocalEvent<CEChangeMaxStaminaComponent, StatusEffectRemovedEvent>(OnRemoved);
+        SubscribeLocalEvent<CEChangeMaxStaminaComponent, CEStatusEffectStackEditedEvent>(OnStackEdited);
+        SubscribeLocalEvent<CEChangeMaxStaminaComponent, StatusEffectRelayedEvent<CECalculateMaxStaminaEvent>>(OnCalculateMaxStamina);
+    }
+
+    private void OnApply(Entity<CEChangeMaxStaminaComponent> ent, ref StatusEffectAppliedEvent args)
+    {
+        _stamina.RefreshMaxStamina(args.Target);
+    }
+
+    private void OnRemoved(Entity<CEChangeMaxStaminaComponent> ent, ref StatusEffectRemovedEvent args)
+    {
+        _stamina.RefreshMaxStamina(args.Target);
+    }
+
+    private void OnStackEdited(Entity<CEChangeMaxStaminaComponent> ent, ref CEStatusEffectStackEditedEvent args)
+    {
+        _stamina.RefreshMaxStamina(args.Target);
+    }
+
+    private void OnCalculateMaxStamina(Entity<CEChangeMaxStaminaComponent> ent,
+        ref StatusEffectRelayedEvent<CECalculateMaxStaminaEvent> args)
+    {
+        var stacks = 1;
+        if (TryComp<CEStatusEffectStackComponent>(ent, out var stackComp))
+            stacks = stackComp.Stacks;
+
+        args.Args.FlatModifier += ent.Comp.FlatChange + ent.Comp.FlatChangePerStack * stacks;
+        args.Args.Multiplier += ent.Comp.MultiplierChange + ent.Comp.MultiplierChangePerStack * stacks;
+    }
+}
